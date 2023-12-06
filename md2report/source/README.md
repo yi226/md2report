ab18994c86a011495d98c2880b9c601a2e0bbf02
alfie <alfiey1017@gmail.com>
2023-12-06 17:23:17 +0800

    update .gitignore

--- md2report/source/README.md
+++
@@ -1,3 +0,0 @@
-# backend
-
-Backend of md2report, including server and md2report CLI.
--- md2report/source/docx_handler/__init__.py
+++
@@ -1,8 +0,0 @@
-from typing import Callable, Dict
-from docx_handler.hust import process_hust_docx
-from docx_handler.general import insert_indent
-
-handler_map: Dict[str, Callable] = {
-    "HUST": process_hust_docx,
-    "first_line_indent": insert_indent,
-}
